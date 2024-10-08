bbbe25398f7efd50d0bbbf532ff4f1a2c5b47993
YuchenHui22314 <yuchen22314@gmail.com>
2024-10-07 22:59:11 -0400

    BFD, corr done

--- utils.py
+++ utils.py
@@ -210,13 +210,12 @@ class bGPTForClassification(PreTrainedModel):
 # Byte Frequency Distribution
 ##########################################################
 
-# code inspired by https://github.com/SCPJaehyeon/byte-frequency-distribution-processing-module
 
 class ByteFrequencyDistribution():
     """Get byte frequency distribution table & shannon entropy results."""
 
     def get_fingerprint_by_folder(self, folder_path):
-        """Get byte frequency distribution table & shannon entropy results.
+        """Get byte frequency distribution table & correlation matrix.
         @param foler_path: folder path.
         @return: fingerprint dict or None.
         """
@@ -242,9 +241,75 @@ class ByteFrequencyDistribution():
         return corr, average_byte_frequency_table
 
 
+    def get_BFD_Corr_plots(self, data_type, input_folder_path, output_folder_path):
+        """Get byte frequency distribution table & correlation matrix.
+        @param data_type: data type, str..
+        @param input_folder_path: input folder path.
+        @param output_folder_path: output folder path.
+        @return: None.
+        """
 
+        corr, average_byte_frequency_table = self.get_fingerprint_by_folder(input_folder_path)
 
+        ##################################################
+        ############# plot correlation matrix
+        ##################################################
 
+        plt.figure(figsize=(15, 12))
+        upper = corr.where(np.triu(np.ones(corr.shape), k=1).astype(bool))
+
+        # all values > 0.5 
+        result = [(column, index) for column in upper.columns for index in upper.index if upper[column][index] > 0.7]
+
+        # variable names
+        variable_names = set([name for pair in result for name in pair])
+
+        #  get all ticks positions
+        all_ticks = np.arange(len(corr.columns))
+
+        #  only keep the ticks of variables with correlation > 0.5
+        filtered_ticks = [i for i, col in enumerate(corr.columns) if col in variable_names]
+
+        # Only display labels greater than 0.5, and set other labels to empty strings
+        xticks_labels = ['' if i not in filtered_ticks else corr.columns[i] for i in all_ticks]
+        yticks_labels = ['' if i not in filtered_ticks else corr.index[i] for i in all_ticks]
+
+        # only show half of the matrix
+        mask = np.triu(np.ones_like(corr, dtype=bool))
+        sns.heatmap(corr, mask = mask, annot=False, fmt=".2f", cmap='coolwarm', linewidths=0.5)
+        # Draw the heatmap with the mask and correct aspect ratio
+        plt.title(f'Correlation Matrix of Datatype [{data_type}]')
+        plt.xticks(ticks=all_ticks, labels=xticks_labels, rotation=45)
+        plt.yticks(ticks=all_ticks, labels=yticks_labels, rotation=45)
+
+        plt.savefig(os.path.join(output_folder_path, f'{data_type}_corr.png'))
+        plt.close()
+
+        #########################################################
+        ############# plot Byte Frequency Distribution matrix
+        #######################################################
+
+        # plot average byte frequency distribution
+        sns.set_style("white")
+        df = pd.DataFrame(list(average_byte_frequency_table.items()), columns=['Byte Value', 'Frequency'])
+        sns.barplot(
+                x='Byte Value',              # x-axis represents byte values (from 00 to FF)
+                y='Frequency',         # y-axis represents their corresponding frequency in percentage
+                data=df,               # data source is our DataFrame created from the frequency table
+                width=1,
+                color='blue',           # color of the bars
+            )
+
+        # Define the specific x-ticks you want to show (e.g., 1, 32, 64, etc.)
+        xticks = list(range(0,256,32)) + [255]
+
+        # Set the x-ticks as well as y-ticks to only show those values
+        plt.xticks(ticks=xticks)
+        plt.yticks(ticks=list(np.arange(0, 1.01, 0.1)))
+
+        plt.title(f'Average Byte Frequency Distribution of {data_type}')
+        plt.savefig(os.path.join(output_folder_path, f'{data_type}_average_byte_frequency_distribution.png'))
+        plt.close()
 
     def get_byte_frequency_table_by_byte_list(self, byte_stream):
         """Get byte frequency table dict.
@@ -281,32 +346,3 @@ class ByteFrequencyDistribution():
         frequency_table, numerical_frequency_table = self.get_byte_frequency_table_by_byte_list(byte_arr)
         return frequency_table, numerical_frequency_table
 
-
-    def _get_shannon_entropy(self, bytefreq):
-        """Calculate shannon entropy(min bits per byte-character).
-        @param bytefreq: byte frequency list.
-        @return: shannon entropy float.
-        """
-        ent = 0.0
-        for freq in bytefreq:
-            freq = freq / 100
-            if freq > 0:
-                ent = ent + freq * math.log(freq, 2)
-        ent = -ent
-        return round(ent, 3)
-
-    def run(self, file_path):
-        """Run byte frequency distribution analysis.
-        @return: analysis results dict or None.
-        """
-        if not os.path.exists(self.file_path):
-            return {}
-
-        try:
-            frequency_table, _ = self.get_byte_frequency_table_by_file_path(file_path)
-            if frequency_table:
-                frequency_table["shannon_entropy"] = self._get_shannon_entropy(frequency_table.values())
-            return frequency_table
-        except Exception as e:
-            print(exception(e))
-            return {}
